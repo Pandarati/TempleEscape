4c0e91b1e6a9851509f86c8f8fa7f5a5d4217660
pandarati <akajonathanmckinley@gmail.com>
2018-11-07 23:21:12 -0800

    Added tile detection between the user and tiles

---
+++ Assets/Scripts/TCDetecto3.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+Gameplan: Have a script file that handles certain type of collisions for
+each type of symbol!
+
+Right now this will handle the: "A" symbol!
+
+*/
+
+public class TCDetecto3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("I started!");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+     {
+				 //Detects when the Sprite goes over a tile
+         if (other.gameObject.name == "UFO")
+         {
+             Debug.Log("Stepped on Tile: A");
+         }
+     }
+
+		 void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider == true)
+        {
+					Debug.Log("2nd Detection detected!");
+				//	collision.collider.enabled = false;
+            Debug.Log("PIzza hut!");
+        }
+    }
+
+}
